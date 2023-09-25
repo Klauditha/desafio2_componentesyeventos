0bb4554dc078d22ed9258f830dbfc236b017fe25
Nibaldo Quezada <nibaldoquezada@hotmail.com>
2023-09-25 17:14:49 -0300

    Respaldo ultima modificacion Formulario

---
+++ src/components/ComponenteError/ComponenteError.jsx
@@ -0,0 +1,7 @@
+import "./ComponenteError.css"
+const ComponenteError = () => {
+    return (
+        <div className='error p-2 m-2 rounded-3'>Completa todos los campos !!</div>
+    )
+}
+export default ComponenteError
--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -2,19 +2,34 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import ComponenteError from '../ComponenteError/ComponenteError';
 
 function Formulario() {
     const [nombre, setNombre] = useState("");
     const [email, setEmail] = useState("");
+    const [vacio, setVacio] = useState(false);
+    const [igual, setIgual] = useState(false);
     const [password, setPassword] = useState("");
-    const [Pass, setPass] = useState("");
+    const [pass, setPass] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        if (nombre === "" || email === "" || password === "" || pass === "") {
+            setVacio(true);
+        } else {
+            setVacio(false)
+        }
+        if (password === pass) {
+            setIgual(true)
+        } else {
+            setIgual(false)
+        }
+
+
     }
     return (
         <>
-            <Form className='justify-content-center align-content-center text-center mb-5' onSubmit={handleSubmit}>
+            <Form className='justify-content-center align-content-center text-center ' onSubmit={handleSubmit}>
 
                 <Form.Group className="mb-3" controlId="formBasicName">
                     <Form.Control placeholder="Nombre" type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
@@ -26,13 +41,13 @@ function Formulario() {
                     <Form.Control placeholder="Contraseña" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control placeholder="Confirma tu contraseña" type="password" value={Pass} onChange={(e) => setPass(e.target.value)} />
+                    <Form.Control placeholder="Confirma tu contraseña" type="password" value={pass} onChange={(e) => setPass(e.target.value)} />
                 </Form.Group>
-
-
                 <Button variant="success" type="submit">
                     Registrarse
                 </Button>
+                {vacio ? <ComponenteError /> : ""}
+
             </Form>
         </>
     )
