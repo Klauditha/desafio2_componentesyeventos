95d66118cb31e9fc73f4b09b81f2114a82e2c36c
Claudia Villarroel <claudia.villarroel@live.com>
2023-09-27 11:14:04 -0300

    ADD: Despliegue dinamico SocialButton

--- .history/src/components/Registro/Registro_20230927002906.jsx
+++ .history/src/components/Registro/Registro_20230927002906.jsx
@@ -27,7 +27,8 @@ const Registro = (props) => {
       <Container className="pt-5 pl-2 pr-2 mb-5 pb-5 bg-light text-center rounded-5">
         <h1>Crea una cuenta</h1>
         <div className="pb-0" id="seccionIconos">
-          {iconosImagenes.map(({ id, value }) => (
+          {iconosImagenes.map(({ id,value }) => (
+            
             <SocialButton key={id} icono={value} />
           ))}
         </div>
